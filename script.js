40a9045dc03c4cddc0fa80d25b43390f5cd67518
CNG <148804493+cicis96@users.noreply.github.com>
2024-01-04 01:18:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function addReview() {
+    const review =
+    document.getElementById("reviewText").value;
+    const addReview =
+    document.getElementById("addedReview");
+    addedReview.innerHTML = review;
+}
